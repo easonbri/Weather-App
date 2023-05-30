b426889f1242c1355d315de504b9ec6865738577
Brianna <brianna.080895@gmail.com>
2023-05-30 13:20:11 -0400

    reconstructing js file

--- wtw.js
+++ wtw.js
@@ -1,53 +1,13 @@
-function format(date) {
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = "0${hours}";
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = "0${minutes}";
-  }
-  let dayindex = date.getDate();
-  let Days = [
+function getDays() {
+  fetch(
+    "6a48a550fc04f170639e60d52b8a6bc5",
+    "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday",
-  ];
-  let day = days[dayindex];
-
-  return "${day} ${hours}:${minutes}";
+    "Saturday"
+  );
 }
-
-function search(event) {
-  event.preventDefalt();
-  let apiKey = "f5029b784306910c19746e40c14d6cd3";
-  let city = "boston";
-  let apiUrl = "https://www.shecodes.io/learn/open_weather_api_keys";
-}
-function convertToFahrenheit(event) {
-  event.preventDefalt();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 66;
-}
-
-function convertToCelsius(event) {
-  event.preventDefalt();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 19;
-}
-
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-dateElement.innerHTML = formateDate(currentTime);
-
-let searchForm = document.querySelector("search-form");
-searchForm.addEventListener("submit", search);
-
-let fahrenheitLink = document.querySelector("fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
-let celsiusLink = document.querySelector("celsius-link");
-celsiusLink.addEventListener("click", convertToCelsius);
+console.log(getDays);
